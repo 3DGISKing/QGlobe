c7f3a8fea0f4091ee2f896ca05e41f7a74b4d4c3
ZhefengJin <wugis1219@gmail.com>
2026-05-09 07:21:12 +0900

    remove QT_LARGEFILE_SUPPORT define

--- launch-gdem.bat
+++ launch-gdem.bat
@@ -7,6 +7,8 @@ set "PATH=%QTDIR%\bin;%PATH%"
 
 if "%~1"=="" (
     start "" "gdem_vs2022.sln"
+	start "" "gdem_server_vs2022.sln"
+	start "" "gdem_client_vs2022.sln"
 ) else (
     start "" "%~1"
 )
